57ee522c8f240bb40f627f509c523df426a6c34e
Avily Silva <avilyre@gmail.com>
2024-08-24 15:47:05 -0300

    test: add search form more tests and improvements

--- src/components/search-form/index.spec.tsx
+++ src/components/search-form/index.spec.tsx
@@ -1,11 +1,15 @@
 import { render, within } from "@testing-library/react";
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import userEvent from "@testing-library/user-event";
 
 import { SearchForm } from ".";
 
 describe("SearchForm Component", () => {
-  const onSearchMockFn = vi.fn();
+  const onSearchMockFn = vi.fn((query: string) => query);
+
+  beforeEach(() => {
+    onSearchMockFn.mockClear();
+  });
 
   it("shoud be able to render", async () => {
     const searchForm = render(<SearchForm onSearch={onSearchMockFn} />);
@@ -41,11 +45,36 @@ describe("SearchForm Component", () => {
     expect(searchInput).toHaveValue("some movie");
   });
 
-  it("should be able to submit the form", async () => {
+  it("should the search form to be submitted with button", async () => {
     const searchForm = render(<SearchForm onSearch={onSearchMockFn} />);
     const submitButton = await searchForm.findByRole("button");
     await userEvent.click(submitButton);
 
-    expect(onSearchMockFn).toHaveBeenCalled();
+    expect(onSearchMockFn).toHaveBeenCalledOnce();
+  });
+
+  it("should the search form to be submitted when the enter key is pressed", async () => {
+    const searchForm = render(<SearchForm onSearch={onSearchMockFn} />);
+    const searchInput = await searchForm.findByRole("searchbox");
+
+    await userEvent.type(searchInput, "some movie");
+    await userEvent.type(searchInput, "{enter}");
+
+    expect(onSearchMockFn).toHaveBeenCalledOnce();
+  });
+
+  it("should be the search form function return with the correct value", async () => {
+    const searchValue = "some great movie";
+
+    const searchForm = render(<SearchForm onSearch={onSearchMockFn} />);
+    const searchInput = searchForm.getByRole("searchbox");
+    
+    await userEvent.type(searchInput, searchValue);
+    await userEvent.type(searchInput, "{enter}");
+
+    const searchFormFnReturnedValue = onSearchMockFn.mock.results.shift()?.value;
+
+    expect(searchFormFnReturnedValue).to.be.equal(searchValue);
+    //or expect(onSearchMockFn).toHaveBeenCalledWith(searchValue);
   });
 });
